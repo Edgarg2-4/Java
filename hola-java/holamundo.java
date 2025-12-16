c29730d28ba6e45ec908d660dc03714ae6436572
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-16 09:09:01 -0600

    se agrego hola-java

---
+++ hola-java/holamundo.java
@@ -0,0 +1,7 @@
+/*Tipo de comentario son parecidos en c */
+
+public class holamundo {
+    public static void main(String[] args) {
+        System.out.println("Me tarde mucho en poder compilar mi pendejada"); // Esta línea se guardará en el archivo
+    }
+}     
