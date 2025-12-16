f5e9b76e006ca8227e3f864d659aa437c05ed01f
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-16 09:14:44 -0600

    se agrego variables

---
+++ variables/Variables.java
@@ -0,0 +1,27 @@
+
+
+public class Variables {
+    public static void main(String[] args) {
+        //Variable
+        String name = "Edgar";
+        System.out.println(name);
+        name = "Rojas";
+        System.out.println(name);
+
+        
+        int age = 23;
+        System.out.println(age);
+
+        //Variable aue se tipea solo
+        var email = "edgar@rojas.com";
+
+        System.out.println(email);
+
+
+        //Constantes
+
+        final String EMAIL = "edgar@gmail.com";
+        System.out.println(EMAIL);
+    }
+    
+}
