a0b97d2a5e983a08c7ff2c56ed99d63a23f2c5bb
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-16 09:04:36 -0600

    Este es el primer commit

---
+++ Condicionales/condicionales.java
@@ -0,0 +1,39 @@
+
+public class condicionales {
+    public static void main(String[] args) {
+        //Condicionales
+        var age = 18;
+        if(age > 18){
+        System.out.println("El usuario es mayor de edad");
+        }else{if(age == 18){
+             System.out.println("El usuario acaba de cumplir 18 años");
+        }else{
+
+            System.out.println("El usuario no es mayor de edad");
+        }
+        }
+
+var day = 6;
+        switch(day){
+
+            case 1:
+               System.out.println("Lunes");
+               break;
+            case 2:
+               System.out.println("Martes");
+               break; 
+            case 3:
+               System.out.println("Miercoles");
+               break; 
+            case 4:
+               System.out.println("Jueves"); 
+               break;
+            case 5:
+               System.out.println("Viernes");
+               break;  
+            default:
+                System.out.println("No es ningun dia");
+        }
+    }
+    
+}
