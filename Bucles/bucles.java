4be29deafe31f63f1db45c2e3eb48ca9fa4292bf
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-18 11:55:05 -0600

    Se agrego while y dowhile

--- Bucles/bucles.java
+++ Bucles/bucles.java
@@ -3,7 +3,8 @@
 public class bucles {
     public static void main(String[] args) {
         //Loops
-
+        
+          String[]names = {"hola", "mundo", "Edgar"};
         //For
         int i;
         for(i=5; i < 5; i+=2){
@@ -12,7 +13,28 @@ public class bucles {
 
         //for each
 
+        //While
+        int index = 0;
+        while(index<5){
+            System.out.println("Hola java");
+            index++;
 
+        }
+        index = 0;
+        while(index<names.length){
+            System.out.println(names[index]);
+            if(names[index].equals("hola")){
+                index+=2;
+            }
+            index++;
+
+        }
+        index = 0;
+        do{
+            System.out.println("Hola java");
+            index++;
+
+        }while(index<0);
     }
     
 }
