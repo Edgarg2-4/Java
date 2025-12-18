d0a10bfa66502286fb17c2d42709fbea00f56633
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-18 09:26:02 -0600

    for

--- Bucles/bucles.java
+++ Bucles/bucles.java
@@ -10,7 +10,13 @@ public class bucles {
             System.out.println("Hola mundo");
         }
 
+        String[] names ={"Edgar", "Rojas", "Guadaraama", "Edgarrg24@gmail.com"};
+
+        for( i= 0; i< names.length ; i++ ){
+            System.out.println(names[i]);
+        }
         //for each
+        
 
 
     }
