311ba2b1d1de72ea81d54800aa0318fac6a5cd55
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-19 09:42:06 -0600

    Funciones con parametros y sobrecarga

--- Funciones/Funciones.java
+++ Funciones/Funciones.java
@@ -2,7 +2,7 @@
 
 public class Funciones {
     public static void main(String[] args) {
-        
+        String[]names = {"hola", "mundo", "Edgar"};
         //Funciones
         for(int i = 0; i<10;i++){
         SendEmail();
@@ -10,11 +10,39 @@ public class Funciones {
 
         SendEmail();
 
+        SendEmailToUser("Edgarrojas@gmail.com");
+
+        SendEmailToUser("Edgarrojas@gmail.com", "Edgar");
+
+        SendEmailToUser(names);
+
     }
 
     public static void SendEmail(){
         System.out.println("Se envia el email");
 
     }
+
+    public static void SendEmailToUser(String email){
+        System.out.println("Se envia el email a "+ email);
+
+    }
+
+    public static void SendEmailToUser(String email, String name){
+        System.out.println("Se envia el email a ("+ email + ") "+ name);
+
+    }
+
+
+     public static void SendEmailToUser(String[] emails){
+        for(String email: emails)
+        System.out.println("Se envia el email a "+ email);
+
+    }
+
+
+   
+    
+    
     
 }
