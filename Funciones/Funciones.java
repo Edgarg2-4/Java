a39ab0e4f74296528fac6d5af11d19ed1dbe3f36
EdgarRG <edgarrg24@comunidad.unam.mx>
2025-12-19 09:27:09 -0600

    Funciones sin retorno y parametro

---
+++ Funciones/Funciones.java
@@ -0,0 +1,20 @@
+
+
+public class Funciones {
+    public static void main(String[] args) {
+        
+        //Funciones
+        for(int i = 0; i<10;i++){
+        SendEmail();
+        }
+
+        SendEmail();
+
+    }
+
+    public static void SendEmail(){
+        System.out.println("Se envia el email");
+
+    }
+    
+}
